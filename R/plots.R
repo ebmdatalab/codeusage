17f1578aeee485aa9672a82e3dc2f8a50ba76685
Milan Wiedemann <milan.wiedemann@gmail.com>
2024-11-15 09:56:03 +0000

    Move main functions from server to R pkg
    This is not very robust but makes the server much lighter, we can build on this structure much easier later.

---
+++ R/plots.R
@@ -0,0 +1,117 @@
+#' Helper function to plot code usage summary
+#' @importFrom ggplot2 ggplot aes geom_line geom_point scale_x_date scale_y_continuous labs theme theme_classic element_text
+#' @importFrom lubridate month year
+#' @importFrom scales label_date_short label_comma comma
+#' @keywords internal
+plot_summary <- function(data) {
+  scale_x_date_breaks <- unique(data$start_date)
+
+  ggplot(
+    data,
+    aes(x = start_date, y = total_usage)
+  ) +
+    geom_line(
+      colour = "#239b89ff",
+      alpha = .4
+    ) +
+    geom_point(
+      colour = "#239b89ff",
+      size = 2,
+      aes(text = paste0(
+        "<b>Timeframe:</b> ",
+        lubridate::month(start_date, label = TRUE), " ",
+        lubridate::year(start_date), " to ",
+        lubridate::month(end_date, label = TRUE), " ",
+        lubridate::year(end_date),
+        "<br>",
+        "<b>Code usage:</b> ", scales::comma(total_usage)
+      ))
+    ) +
+    scale_x_date(
+      breaks = scale_x_date_breaks,
+      labels = scales::label_date_short()
+    ) +
+    scale_y_continuous(
+      limits = c(0, NA),
+      labels = scales::label_comma()
+    ) +
+    labs(x = NULL, y = NULL) +
+    theme_classic() +
+    theme(text = element_text(size = 14))
+}
+
+#' Helper function to plot individual code usage
+#' @importFrom ggplot2 ggplot aes geom_line geom_point scale_x_date scale_y_continuous labs theme theme_classic element_text
+#' @importFrom lubridate month year
+#' @importFrom scales label_date_short label_comma comma
+#' @keywords internal
+plot_individual <- function(data) {
+  scale_x_date_breaks <- unique(data$start_date)
+
+  ggplot(
+    data,
+    aes(
+      x = start_date,
+      y = usage,
+      colour = code
+    )
+  ) +
+    geom_line(alpha = .4) +
+    geom_point(
+      size = 2,
+      aes(text = paste0(
+        "<b>Timeframe:</b> ",
+        lubridate::month(start_date, label = TRUE), " ",
+        lubridate::year(start_date), " to ",
+        lubridate::month(end_date, label = TRUE), " ",
+        lubridate::year(end_date),
+        "<br>",
+        "<b>Code:</b> ", code, "<br>",
+        "<b>Description:</b> ", description, "<br>",
+        "<b>Code usage:</b> ", scales::comma(usage)
+      ))
+    ) +
+    scale_x_date(
+      breaks = scale_x_date_breaks,
+      labels = scales::label_date_short()
+    ) +
+    scale_y_continuous(
+      limits = c(0, NA),
+      labels = scales::label_comma()
+    ) +
+    ggplot2::scale_colour_viridis_d() +
+    labs(x = NULL, y = NULL) +
+    theme_classic() +
+    theme(
+      text = element_text(size = 14),
+      legend.position = "none"
+    )
+}
+
+#' Helper function to plot sparkline
+#' @importFrom dplyr group_by summarise
+#' @importFrom plotly plot_ly add_lines layout config
+#' @keywords internal
+plot_sparkline <- function(data) {
+  data_spark <- data |>
+    group_by(start_date) |>
+    summarise(total_usage = sum(usage, na.rm = TRUE))
+
+  plot_ly(data_spark, hoverinfo = "none") |>
+    add_lines(
+      x = ~start_date, y = ~total_usage,
+      color = I("black"), span = I(1),
+      fill = "tozeroy", alpha = 0.2
+    ) |>
+    layout(
+      xaxis = list(visible = F, showgrid = F, title = ""),
+      yaxis = list(visible = F, showgrid = F, title = ""),
+      hovermode = "x",
+      margin = list(t = 0, r = 0, l = 0, b = 0),
+      font = list(color = "white"),
+      paper_bgcolor = "transparent",
+      plot_bgcolor = "transparent"
+    ) |>
+    config(displayModeBar = FALSE) |>
+    layout(xaxis = list(fixedrange = TRUE), yaxis = list(fixedrange = TRUE))
+}
