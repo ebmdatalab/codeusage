a37d259ed8e40d08e15b261ba7a2fcc929c99bda
Arina Tamborska <143546177+atamborska@users.noreply.github.com>
2025-01-09 17:22:57 +0000

    Atamborska/show proportions (#34)
    Show proportions in individual breakage graph
    Add examples of use to ICD-10 and OPCS-4
    ---------
    
    Co-authored-by: Milan Wiedemann <milan.wiedemann@gmail.com>

--- R/data.R
+++ R/data.R
@@ -32,36 +32,52 @@ NULL
 
 #' Yearly ICD-10 Code Usage from Hospital Admitted Patient Care Activity in England
 #'
-#' Yearly summary of ICD-10 code usage from 1st April 2013 to 31st March 2024.
-#' All diagnosis codes are summarised to the 4-character level.
-#' The code usage represents the total annual count of episodes which record in any primary or secondary position.
-#' Restricted codes for which annual usage is not published, have been removed.
+#' Yearly summary of 4-character ICD-10 code usage from 1st April 2013 to 31st March 2024.
+#' The code usage represents the annual count of all episodes which record the given ICD-10 code in any primary or secondary position.
+#' Restricted codes for which annual usage is not published have been removed.
 #' @format A data frame with 135,951 rows and 5 columns:
 #' \describe{
 #'   \item{start_date}{Start date of code usage count}
 #'   \item{end_date}{End date of code usage count}
 #'   \item{icd10_code}{The 4-character ICD-10 Code.
 #'   Note that the punctuation from the code has been removed for compatibility with OpenCodelists.}
-#'   \item{usage}{Yearly summary of code usage.}
-#'   \item{description}{Description of ICD-10 Code}
+#'   \item{usage}{Annual count of code usage.}
+#'   \item{description}{Description of the ICD-10 Code}
 #' }
 #' @source <https://digital.nhs.uk/data-and-information/publications/statistical/hospital-admitted-patient-care-activity>
+#' @examples
+#' # Filter to codes in the ICD-10 Chapter XIX: "Injury, poisoning..." 
+#' # (codes begin with letters "S" or "T"), with usage > 10,000.
+#' # For each of these, select the year with the highest count. 
+#' icd10_usage |>
+#' dplyr:: filter(grepl("^[ST]", icd10_code) & usage > 10000)|>
+#' dplyr:: group_by(description) |>
+#' dplyr:: slice_max(usage)
+#' # Filter to codes present in the CPRD Aurum ICD-10 pregnancy codelist.
+#' # This codelist is available in OpenCodelists.org
+#' codelist<- read.csv(
+#' "https://www.opencodelists.org/codelist/opensafely/pregnancy-icd10-aurum/5a7d8d12/download.csv")
+#' icd10_usage |>
+#' dplyr:: filter(icd10_code %in% codelist$code) 
 "icd10_usage"
 
 #' Yearly OPCS Code Usage from Hospital Admitted Patient Care Activity in England
 #'
-#' Yearly summary of OPCS code usage from 1st April 2013 to 31st March 2024.
-#' All procedure codes are summarised to the 4-character level.
-#' The code usage represents total annual count of each procedure, recorded across the primary and the secondary procedure positions.
-#' Restricted codes for which annual usage is not published, have been removed.
+#' Yearly summary of 4-character OPCS code usage from 1st April 2013 to 31st March 2024.
+#' The code usage represents the total annual count of each procedure, recorded across the primary and the secondary procedure positions.
+#' Restricted codes for which annual usage is not published have been removed.
 #' @format A data frame with 107,376 rows and 5 columns:
 #' \describe{
 #'   \item{start_date}{Start date of code usage count}
 #'   \item{end_date}{End date of code usage count}
 #'   \item{opcs_code}{The 4-character OPCS code.
 #'   Note that the punctuation from the code has been removed for compatibility with OpenCodelists.}
-#'   \item{usage}{Yearly summary of code usage.}
-#'   \item{description}{Description of OPCS Code}
+#'   \item{usage}{Annual count of code usage.}
+#'   \item{description}{Description of the OPCS Code}
 #' }
 #' @source <https://digital.nhs.uk/data-and-information/publications/statistical/hospital-admitted-patient-care-activity>
+#' @examples
+#' # Filter to procedures involving "biopsy" after March 2020 (note each year runs April - March).
+#' opcs_usage |>
+#' dplyr:: filter(grepl("biopsy", description, ignore.case = TRUE) & lubridate:: year(end_date) > 2020)
 "opcs_usage"
